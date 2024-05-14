ca68f8dc9fc276a3e788207d27c27a766ddaabed
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-05-14 10:04:09 +0900

    Title: 중앙값 구하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240514/중앙값 구하기/find-the-median.py
+++ 240514/중앙값 구하기/find-the-median.py
@@ -2,32 +2,46 @@ r = input()
 arr = r.split()
 a = int(arr[0])
 b = int(arr[1])
-c = int(arr[1])
+c = int(arr[2])
 
+# 세 수를 리스트에 넣고 정렬
+# numbers = [a, b, c]
+# numbers.sort()
 
-if a > b and a < c:
-    print(a)
-elif a > b and a > c:
-    print(a)
-elif a < b and a > c:
-    print(a)
-elif a < b and a < c:
-    print(a)
+# 정렬된 리스트의 중간 인덱스인 1의 값을 출력
+# print(numbers[1])
 
-elif b > a and b < c:
+if (b <= a <= c) or (c <= a <= b):
+    print(a)
+elif (a <= b <= c) or (c <= b <= a):
     print(b)
-elif b < a and b > c:
-    print(b)     
-elif b < a and b < c:
-    print(b)
-elif b < a and b < c:
-    print(b)    
+else:
+    print(c)
 
-elif c > a and c < b:
-    print(c)
-elif c < a and c > b:
-    print(c)
-elif c > a and c < b:
-    print(c)
-elif c < a and c < b:
-    print(c)
+
+# if a > b and a < c:
+#     print(a)
+# elif a > b and a > c:
+#     print(a)
+# elif a < b and a > c:
+#     print(a)
+# elif a < b and a < c:
+#     print(a)
+
+# elif b > a and b < c:
+#     print(b)
+# elif b < a and b > c:
+#     print(b)     
+# elif b < a and b < c:
+#     print(b)
+# elif b < a and b < c:
+#     print(b)    
+
+# elif c > a and c < b:
+#     print(c)
+# elif c < a and c > b:
+#     print(c)
+# elif c > a and c < b:
+#     print(c)
+# elif c < a and c < b:
+#     print(c)
