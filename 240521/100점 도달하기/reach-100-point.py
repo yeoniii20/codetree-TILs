3c8d4ff8450adc39de9c47221b60f68fa0c6050b
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-05-21 11:30:26 +0900

    Title: 100점 도달하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240521/100점 도달하기/reach-100-point.py
+++ 240521/100점 도달하기/reach-100-point.py
@@ -2,12 +2,18 @@ n = int(input())
 
 for i in range(n, 101):
     if (90 <= i):
-        print("A") 
-    elif (89 <= i and 80 <= i):
-        print("B")
-    elif (79 <= i and 70 <= i):
-        print("C")
-    elif (69 <= i and 60 <= i):
-        print("D")
+        a = "A"
+        # print("A") 
+    elif (i <= 89 and 80 <= i):
+        a = "B"
+        # print("B")
+    elif (i <= 79 and 70 <= i):
+        a = "C"
+        # print("C")
+    elif (i <= 69 and 60 <= i):
+        a = "D"
+        # print("D")
     elif (i < 60):
-        print("F")
+        a = "F"
+        # print("F")
+    print(a, end=" ")
