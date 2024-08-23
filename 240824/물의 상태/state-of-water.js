53646ce1891d696d9e03e93f2046490aef5329bf
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-24 08:04:19 +0900

    Title: 물의 상태, Time: 174ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240824/물의 상태/state-of-water.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a < 0) {
+    console.log("ice")
+} else if (a >= 100) {
+    console.log("vapor") 
+} else {
+    console.log("water")
+}
