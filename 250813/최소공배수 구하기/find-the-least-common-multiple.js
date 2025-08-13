80f6342200dd5ae47077373119de8924f257c43a
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-08-13 10:06:53 +0900

    Title: 최소공배수 구하기, Time: 176ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250813/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+
+let minNum = 0
+let arr = []
+
+function minNumber(n, m) {
+    for(let i = 1; i <=n; i++){
+        if (n % i === m % i) {
+        arr.push(i)
+        }
+    }
+    minNum = Math.max(...arr)
+
+    const min1 = n / minNum
+    const min2 = m / minNum
+
+    console.log(min1 * min2 * minNum)
+
+}
+
+minNumber(n, m)
+
+// 먼저 최소 공배수를 구한 다음, 
+// 최소 공배수로 각각의 수를 나눈 몫을 곱함
